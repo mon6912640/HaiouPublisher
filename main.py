c370109fc1f2b430b703dd423dd8f1f8e528807b
lumaliu <010monkey@163.com>
2021-02-02 10:38:33 +0800

    导出配置功能

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import subprocess
 import time
 from functools import partial
+import xlrd
+from typing import List
+import json
 
 import requests
 from pywebio import start_server
@@ -12,8 +15,9 @@ from bytes_util import *
 from haiou_protocol import VoProtocol
 from pathlib import Path
 
-root = 'I:/newQz/client/yxqz/'
-url = 'http://192.168.61.142:8080/ProtocolNewQZ/'
+root_work = 'I:/newQz/client/yxqz/'
+url_proto = 'http://192.168.61.142:8080/ProtocolNewQZ/'
+cfg_source = 'I:/newQz/策划/配置表/'
 
 
 def log(content, scope=None):
@@ -21,6 +25,10 @@ def log(content, scope=None):
     run_js(
         '$("#pywebio-scope-content>div").stop().animate({ scrollTop: $("#pywebio-scope-content>div").prop("scrollHeight")}, 1000)')  # hack: to scroll bottom
     # pprint(content)
+
+
+def error(content):
+    log('<font color="#ff0000">{0}</font>'.format(content))
 
 
 def print_now():
@@ -45,19 +53,19 @@ def run_cmd(cmd, err_str):
 def build(btn_val):
     log(print_now())
     log(">>开始编译...请耐心等待...")
-    run_cmd('egret clean {0} -sourcemap'.format(root), '编译错误')
+    run_cmd('egret clean {0} -sourcemap'.format(root_work), '编译错误')
 
 
 def update(btn_val):
     log(print_now())
     log(">>开始更新...请耐心等待...")
-    run_cmd('svn up {0}'.format(root), '更新错误')
+    run_cmd('svn up {0}'.format(root_work), '更新错误')
 
 
-def test(btn_val):
+def protocol(btn_val):
     log(print_now())
     log(">>导出协议...请耐心等待...")
-    response = requests.get(url + '/protocol.do?method=proExtExport')
+    response = requests.get(url_proto + '/protocol.do?method=proExtExport')
     # 返回二进制流
     by = response.content
     # log('二进制长度=' + str(len(by)))
@@ -90,16 +98,129 @@ def test(btn_val):
         bin_by += bytes_util.write_utf(vo.get_protocol_variable())
 
     bin_by = bytes_util.write_int(proto_count) + bin_by
-    bin_path = Path(root, 'resource/config/clientProtocol.bin')
+    bin_path = Path(root_work, 'resource/config/clientProtocol.bin')
     with bin_path.open('wb') as fs:
         fs.write(bin_by)
 
-    code_path = Path(root, 'src/protocol/IProtocol0.ts')
+    code_path = Path(root_work, 'src/protocol/IProtocol.ts')
     with code_path.open('wt', encoding='utf-8') as fs:
         fs.write(info)
 
     log('>>...协议生成完毕')
-    pass
+
+
+class VoKey:
+    col_index = 0
+    key_type = ''
+    name = ''
+    des = ''
+
+    def parse_value(self, cell):
+        if self.key_type == 'INT':
+            if cell.ctype == 2:  # number
+                if cell.value % 1 == 0.0:
+                    value = int(cell.value)
+                else:
+                    value = cell.value
+            else:
+                value = 0
+        else:
+            if cell.ctype == 2:
+                if cell.value % 1 == 0.0:
+                    value = str(int(cell.value))
+                else:
+                    value = str(cell.value)
+            else:
+                value = str(cell.value)
+        return value
+
+
+class VoCfg:
+    # 导出的配置名
+    cfg_name = ''
+    # 配置源文件名
+    fname = ''
+    key_list: List[VoKey] = None
+
+    def __init__(self):
+        self.key_list = []
+
+
+def pack_cfg(btn_val):
+    log(print_now())
+    log(">>开始打包配置...请耐心等待...")
+    source_path = Path(cfg_source)
+    list_file = sorted(source_path.rglob('*.xlsx'))
+    vo_cfg_map = {}
+    obj_map = {}
+    for v in list_file:
+        file_url = v.absolute()
+        if v.name.find('~$') > -1:  # 跳过临时打开的xlsx文件
+            continue
+        wb = xlrd.open_workbook(filename=file_url)
+        sheet = wb.sheet_by_index(0)
+        if sheet.cell_type(0, 0) != 1 or sheet.cell_value(0, 0).upper() == 'NO':
+            continue
+        if sheet.cell_type(0, 0) != 1:
+            error('{0} 没有导出表名，不能导出'.format(v.name))
+            continue
+        row_client = sheet.row(0)
+        row_key_name = sheet.row(2)
+        row_key_des = sheet.row(3)
+        cfg_name = row_key_name[0].value
+        if cfg_name in vo_cfg_map:
+            error('FBI warning，发现重复表，提交者：死肥仔；表名冲突者：[{0}]与[{1}]'.format(vo_cfg_map[cfg_name].fname, v.name))
+            continue
+        vo_cfg_map[cfg_name] = vo = VoCfg()
+        obj_map[cfg_name] = obj = {'key': [], 'data': []}
+        vo.cfg_name = cfg_name
+        vo.fname = v.name
+        # count = 0
+        for i in range(1, len(row_client)):
+            if row_client[i].ctype != 1:
+                continue
+            key_type = row_client[i].value.upper()
+            if key_type == 'NO':
+                continue
+            key_vo = VoKey()
+            key_vo.col_index = i
+            key_vo.key_type = key_type
+            key_vo.name = row_key_name[i].value
+            key_vo.des = row_key_des[i].value
+            vo.key_list.append(key_vo)
+            obj['key'].append(key_vo.name)
+            # count += 1
+
+        for i in range(4, sheet.nrows):
+            # 首列为0的行不导出
+            cell0 = sheet.cell(i, 0)
+            if cell0.ctype == 2:  # number
+                if cell0.value % 1 == 0.0:
+                    value0 = int(cell0.value)
+                else:
+                    value0 = cell0.value
+            else:
+                value0 = 0
+            if value0 == 0:
+                continue
+
+            value_list = []
+            for kv in vo.key_list:
+                try:
+                    value = kv.parse_value(sheet.cell(i, kv.col_index))
+                except:
+                    error('出错 {0} {1}行{2}列'.format(v.name, i, kv.col_index))
+                else:
+                    value_list.append(value)
+            obj['data'].append(value_list)
+        # break
+
+    # print(obj_map)
+    json_str = json.dumps(obj_map, ensure_ascii=False, separators=(',', ':'))
+    json_path = Path(root_work, 'resource/config/config1.json')
+    with json_path.open('w', encoding='utf-8') as fs:
+        fs.write(json_str)
+    log('>>...配置打包完毕')
 
 
 async def main():
@@ -110,9 +231,9 @@ async def main():
     put_table([
         ['新枪战', ''],
         ['更新资源和代码', put_buttons(['更新'], onclick=partial(update))],
-        ['导出协议', put_buttons(['协议'], onclick=partial(test))],
+        ['导出协议', put_buttons(['协议'], onclick=partial(protocol))],
         ['编译代码', put_buttons(['编译'], onclick=partial(build))],
-        ['打包配置', put_buttons(['打包'], onclick=...)],
+        ['打包配置', put_buttons(['打包'], onclick=partial(pack_cfg))],
         [put_link('版本服地址（新枪战）', url='http://192.168.61.64:5555/index.html', new_window=True), '']
     ])
 
